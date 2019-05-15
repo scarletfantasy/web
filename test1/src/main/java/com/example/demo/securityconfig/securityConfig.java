3852f3ebed283d82f4ecc0f129ab44421b273565
scarletfantasy <40877197+scarletfantasy@users.noreply.github.com>
2019-05-15 10:06:09 +0800

    second rewrite
    update readme

--- test1/src/main/java/com/example/demo/securityconfig/securityConfig.java
+++ test1/src/main/java/com/example/demo/securityconfig/securityConfig.java
@@ -27,7 +27,7 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorize)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/jpalogin","/sslogin","/jpabooklist","/jparegister","/**").permitAll()
+                .antMatchers("/jpalogin","/sslogin","/jpabooklist","/jparegister","/img/*").permitAll()
                 .antMatchers("/jpashowcart","/jpacleancart","/jpaaddtocart","/jpashowhistory","/jpadeleteorder").hasAnyAuthority("USER","ADMIN")
                 .antMatchers("/jpaeditsave","/jpaeditdelete","/jpashowuser").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
